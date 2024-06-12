f7c6c408b905d0a706ef6212f97bc689463a5641
umid2401 <umamatraximov@gmail.com>
2024-06-13 03:59:21 +0500

    changes

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,9 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "../Styles/Article.scss";
-
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
 export default function Article() {
-  const [check, setCheck]= useState(false);
+  const position = [38.266, 67.9];
+  const customIcon = new Icon({
+    iconUrl: "Images/location.png",
+    iconSize: [40, 40],
+    popupAnchor: [-0, -76],
+  });
+  const [check, setCheck] = useState(false);
   return (
     <div>
       <header className="header">
@@ -47,47 +55,62 @@ export default function Article() {
               </div>
               <div className="hide">
                 <img src="Images/phone.png" alt="" />
-                {check?(<img onClick={setCheck(!check)} src="Images/Icon (1).png" alt="" />):( <img onClick={setCheck(!check)} src="Images/menu.png" alt="" />)}
-               
-                
-
+                {!check ? (
+                  <img
+                    onClick={() => setCheck(!check)}
+                    src="Images/menu.png"
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    onClick={() => setCheck(!check)}
+                    src="Images/Icon (1).png"
+                    alt=""
+                  />
+                )}
               </div>
             </div>
           </div>
         </div>
-        <div className="mobile-menu">
-          <div className="menu">
-          <div className="tank">
-            <h2>АВТОРУСЬ TANK</h2>
-            <p>Официальный дилер</p>
-          </div>
-          <div className="number">
-            <h3>+7 (999) 999-99-99</h3>
-            <p> Мы на связи</p>
-          </div>
-          <div className="location">
-           <div className="img"> <img src="Images/location.png" alt="" /></div>
-            <p>Ярославское шоссе, владение 2 В, строение 3 (МКАД, 95 км)</p>
-          </div>
-          <div className="bottom">
-          <div className="button">
-            <button>
-              <img src="Images/Icon.png" alt="" />
-              <span>Заказать звонок</span>
-            </button>
-          </div>
-            <div className="service">
-              <img src="Images/settings.png" alt="" />
-              <h3>Записаться на сервис</h3>
-            </div>
-            <div className="test">
+        {check ? (
+          <div className="mobile-menu">
+            <div className="menu">
+              <div className="tank">
+                <h2>АВТОРУСЬ TANK</h2>
+                <p>Официальный дилер</p>
+              </div>
+              <div className="number">
+                <h3>+7 (999) 999-99-99</h3>
+                <p> Мы на связи</p>
+              </div>
+              <div className="location">
+                <div className="img">
+                  {" "}
+                  <img src="Images/location.png" alt="" />
+                </div>
+                <p>Ярославское шоссе, владение 2 В, строение 3 (МКАД, 95 км)</p>
+              </div>
+              <div className="bottom">
+                <div className="button">
+                  <button>
+                    <img src="Images/Icon.png" alt="" />
+                    <span>Заказать звонок</span>
+                  </button>
+                </div>
+                <div className="service">
+                  <img src="Images/settings.png" alt="" />
+                  <h3>Записаться на сервис</h3>
+                </div>
+                <div className="test">
                   <img src="Images/settings.png" alt="" />
                   <h3>Тест-драйв</h3>
                 </div>
+              </div>
+            </div>
           </div>
-          </div>
-         
-        </div>
+        ) : (
+          ""
+        )}
       </header>
       <section className="main">
         <div className="main-wrapper">
@@ -99,11 +122,24 @@ export default function Article() {
           </div>
         </div>
       </section>
+      <section className="hide">
+        <div className="text">
+          <p>Улучшим любые условия</p>
+          <h1>TANK 500</h1>
+          <p>Осталось всего 5 автомобилей!</p>
+          <button>Получить предложение</button>
+        </div>
+      </section>
       <section className="frame">
         <div className="frame-wrapper">
           <div className="time-content">
             <h2>Срок действия спецпредложения:</h2>
-            <div className="time"></div>
+            <div className="time">
+              <div className="day box">3</div>
+              <div className="hours box">13</div>
+              <div className="minut box">34</div>
+              <div className="second box">12</div>
+            </div>
             <button>Узнать цену с выгодами</button>
           </div>
           <div className="cards">
@@ -154,6 +190,213 @@ export default function Article() {
               <div>
                 <img src="Images/settings.png" alt="" />
                 <h3>Тест-драйв</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="complect">
+        <div className="wrapper">
+          <h2>ПОДБЕРИТЕ КОМПЛЕКТАЦИЮ</h2>
+          <div className="cards">
+            <div className="card">
+              <div className="img">
+                <img src="Images/image1.png" alt="" />
+              </div>
+              <div className="text">
+                <div className="top-text">
+                  <h2>TANK 300</h2>
+                  <p>2.0 л. Бензин, Полный, Автоматическая</p>
+                </div>
+                <div className="center-text">
+                  <h2>3 649 000 ₽</h2>
+                </div>
+                <div className="bottom-text">
+                  <div className="line-text">
+                    <img src="Images/Icon (2).png" alt="" />
+                    <p>Выгода по Trade-in до 450 000 ₽</p>
+                  </div>
+                  <div className="line-text">
+                    <img src="Images/Icon (3).png" alt="" />
+                    <p>Керамическое покрытие в подарок!</p>
+                  </div>
+                </div>
+              </div>
+              <div className="button-group">
+                <button>
+                  <span>Получить предложение</span>
+                  <img src="Images/chevron-right.png" alt="" />
+                </button>
+                <button>
+                  <span>Тест-драйв</span>
+                  <img src="Images/chevron-right (1).png" alt="" />
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="Images/image1.png" alt="" />
+              </div>
+              <div className="text">
+                <div className="top-text">
+                  <h2>TANK 300</h2>
+                  <p>2.0 л. Бензин, Полный, Автоматическая</p>
+                </div>
+                <div className="center-text">
+                  <h2>3 649 000 ₽</h2>
+                </div>
+                <div className="bottom-text">
+                  <div className="line-text">
+                    <img src="Images/Icon (2).png" alt="" />
+                    <p>Выгода по Trade-in до 450 000 ₽</p>
+                  </div>
+                  <div className="line-text">
+                    <img src="Images/Icon (3).png" alt="" />
+                    <p>Керамическое покрытие в подарок!</p>
+                  </div>
+                </div>
+              </div>
+              <div className="button-group">
+                <button>
+                  <span>Получить предложение</span>
+                  <img src="Images/chevron-right.png" alt="" />
+                </button>
+                <button>
+                  <span>Тест-драйв</span>
+                  <img src="Images/chevron-right (1).png" alt="" />
+                </button>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="Images/image1.png" alt="" />
+              </div>
+              <div className="text">
+                <div className="top-text">
+                  <h2>TANK 300</h2>
+                  <p>2.0 л. Бензин, Полный, Автоматическая</p>
+                </div>
+                <div className="center-text">
+                  <h2>3 649 000 ₽</h2>
+                </div>
+                <div className="bottom-text">
+                  <div className="line-text">
+                    <img src="Images/Icon (2).png" alt="" />
+                    <p>Выгода по Trade-in до 450 000 ₽</p>
+                  </div>
+                  <div className="line-text">
+                    <img src="Images/Icon (3).png" alt="" />
+                    <p>Керамическое покрытие в подарок!</p>
+                  </div>
+                </div>
+              </div>
+              <div className="button-group">
+                <button>
+                  <span>Получить предложение</span>
+                  <img src="Images/chevron-right.png" alt="" />
+                </button>
+                <button>
+                  <span>Тест-драйв</span>
+                  <img src="Images/chevron-right (1).png" alt="" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <button className="button">Загрузить еще</button>
+          <div className="boxes">
+            <div className="img">
+              <img src="Images/image 290.png" alt="error" />
+            </div>
+            <div className="text">
+              <h2>ОБМЕН ПО TRADE-IN НА ВЫГОДНЫХ УСЛОВИЯХ</h2>
+              <button>
+                <span>Отправить заявку</span>
+                <img src="Images/chevron-right.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="credit">
+        <div className="wrapper">
+          <div className="text">
+            <h2>Оставьте заявку на кредит</h2>
+            <p>и получите одобрение за 1 день!</p>
+            <form action="">
+              <div className="btn-group">
+                <label htmlFor=""></label>
+                <input type="number" placeholder="+7(.....)" />
+                <button>
+                  <span>Отправить заявку</span>
+                  <img src="Images/chevron-right.png" alt="" />
+                </button>
+              </div>
+              <input type="checkbox" />
+              <label htmlFor="">
+                Согласен на обработку персональных данных.
+              </label>
+            </form>
+            <p></p>
+          </div>
+        </div>
+      </section>
+      <section className="contact">
+        <div className="wrapper">
+          <div className="profile">
+            <div className="profile-left">
+              <h2>Индивидуальное предложение для корпоративных клиентов</h2>
+              <button>
+                <span>Отправить заявку</span>
+                <img src="Images/chevron-right.png" alt="" />
+              </button>
+            </div>
+            <div className="profile-right">
+              <div className="img">
+                <img src="Images/image 295.png" alt="" />
+              </div>
+              <div className="text">
+                <h2>Татьяна Санникова</h2>
+                <p>Отдел корпоративных продаж АВТОРУСЬ</p>
+              </div>
+            </div>
+          </div>
+          <div className="maps">
+            <div className="map">
+              <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Denov A.Navoiy</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position} icon={customIcon}>
+                  <Popup position={position}>
+                    Marhamat joylashuvimiz bilan tanishing!!
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+            <div className="map-detail">
+              <div className="cards">
+                <div className="card">
+                  <p>Адрес</p>
+                  <h3>
+                    Ярославское шоссе, владение 2 В, строение 3 (МКАД, 95 км)
+                  </h3>
+                </div>
+                <div className="card">
+                  <p>Телефон</p>
+                  <h3>+7 (999) 999-99-99</h3>
+                  <p>Мы на связи</p>
+                </div>
+                <div className="card">
+                  <p>Режим работы</p>
+                  <h3>Ежедневно с 09:00 до 21:00</h3>
+                </div>
+                <div className="card">
+                  <button>
+                  <span>Заказать звонок</span>
+                  <img src="Images/chevron-right.png" alt="" />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
